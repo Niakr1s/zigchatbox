aa40912a6ed2df40ce935097eac8b8f2ba73fac5
Niakr1s <pavel2188@gmail.com>
2026-06-21 14:12:00 +0300

    moved Msg and Cmd inside

--- src/tokens.zig
+++ src/tokens.zig
@@ -1,76 +1,9 @@
 const std = @import("std");
 
-/// A simple message line got from client
-pub const ClientMsg = struct {
-    msg: []const u8,
-
-    fn deinit(self: ClientMsg, gpa: std.mem.Allocator) void {
-        gpa.free(self.msg);
-    }
-
-    fn fromStringAlloc(gpa: std.mem.Allocator, str: []const u8) !ClientMsg {
-        const msg = try gpa.dupe(u8, str);
-        return ClientMsg{
-            .msg = msg,
-        };
-    }
-};
-
-pub const ClientCmd = union(enum) {
-    whoami: Whoami,
-    nickname: Nickname,
-
-    fn deinit(self: ClientCmd, gpa: std.mem.Allocator) void {
-        switch (self) {
-            .whoami => {},
-            .nickname => {
-                self.nickname.deinit(gpa);
-            },
-        }
-    }
-
-    /// Requests nickname
-    pub const Whoami = struct {
-        const CMD = "whoami";
-    };
-
-    /// Changes nick
-    pub const Nickname = struct {
-        nickname: []const u8,
-
-        const CMD = "nickname";
-
-        fn deinit(self: Nickname, gpa: std.mem.Allocator) void {
-            gpa.free(self.nickname);
-        }
-
-        fn init(gpa: std.mem.Allocator, nickname: []const u8) !Nickname {
-            return Nickname{
-                .nickname = try gpa.dupe(u8, nickname),
-            };
-        }
-    };
-
-    /// Args:
-    ///   str: this should be a trimmed string without command prefix
-    ///   so, if a command: "/whoami", caller should trim the first char
-    fn fromStringAlloc(gpa: std.mem.Allocator, str: []const u8) !ClientCmd {
-        // std.debug.print("{s}\n", .{str});
-        // std.debug.print("cut prefix: {s}: in {s}: {any}\n", .{ Nickname.CMD ++ " ", str, std.mem.cutPrefix(u8, str, Nickname.CMD ++ " ") });
-        if (std.mem.eql(u8, Whoami.CMD, str)) {
-            return ClientCmd{ .whoami = .{} };
-        } else if (std.mem.cutPrefix(u8, str, Nickname.CMD ++ " ")) |nickname| {
-            return ClientCmd{ .nickname = try Nickname.init(gpa, nickname) };
-        } else {
-            return error.UnknownClientCmd;
-        }
-    }
-};
-
 /// Represents a token, that client sends
 pub const ClientToken = union(enum) {
-    msg: ClientMsg,
-    cmd: ClientCmd,
+    msg: Msg,
+    cmd: Cmd,
 
     pub fn deinit(self: ClientToken, gpa: std.mem.Allocator) void {
         switch (self) {
@@ -88,38 +21,105 @@ pub const ClientToken = union(enum) {
         }
 
         if (trimmed[0] == '/') {
-            const cmd = try ClientCmd.fromStringAlloc(gpa, trimmed[1..]);
+            const cmd = try Cmd.fromStringAlloc(gpa, trimmed[1..]);
             return ClientToken{
                 .cmd = cmd,
             };
         } else {
-            const msg = try ClientMsg.fromStringAlloc(gpa, trimmed);
+            const msg = try Msg.fromStringAlloc(gpa, trimmed);
             return ClientToken{
                 .msg = msg,
             };
         }
     }
+
+    /// A simple message line got from client
+    pub const Msg = struct {
+        msg: []const u8,
+
+        fn deinit(self: Msg, gpa: std.mem.Allocator) void {
+            gpa.free(self.msg);
+        }
+
+        fn fromStringAlloc(gpa: std.mem.Allocator, str: []const u8) !Msg {
+            const msg = try gpa.dupe(u8, str);
+            return Msg{
+                .msg = msg,
+            };
+        }
+    };
+
+    pub const Cmd = union(enum) {
+        whoami: Whoami,
+        nickname: Nickname,
+
+        fn deinit(self: Cmd, gpa: std.mem.Allocator) void {
+            switch (self) {
+                .whoami => {},
+                .nickname => {
+                    self.nickname.deinit(gpa);
+                },
+            }
+        }
+
+        /// Requests nickname
+        pub const Whoami = struct {
+            const CMD = "whoami";
+        };
+
+        /// Changes nick
+        pub const Nickname = struct {
+            nickname: []const u8,
+
+            const CMD = "nickname";
+
+            fn deinit(self: Nickname, gpa: std.mem.Allocator) void {
+                gpa.free(self.nickname);
+            }
+
+            fn init(gpa: std.mem.Allocator, nickname: []const u8) !Nickname {
+                return Nickname{
+                    .nickname = try gpa.dupe(u8, nickname),
+                };
+            }
+        };
+
+        /// Args:
+        ///   str: this should be a trimmed string without command prefix
+        ///   so, if a command: "/whoami", caller should trim the first char
+        fn fromStringAlloc(gpa: std.mem.Allocator, str: []const u8) !Cmd {
+            // std.debug.print("{s}\n", .{str});
+            // std.debug.print("cut prefix: {s}: in {s}: {any}\n", .{ Nickname.CMD ++ " ", str, std.mem.cutPrefix(u8, str, Nickname.CMD ++ " ") });
+            if (std.mem.eql(u8, Whoami.CMD, str)) {
+                return Cmd{ .whoami = .{} };
+            } else if (std.mem.cutPrefix(u8, str, Nickname.CMD ++ " ")) |nickname| {
+                return Cmd{ .nickname = try Nickname.init(gpa, nickname) };
+            } else {
+                return error.UnknownClientCmd;
+            }
+        }
+    };
 };
 
 test "ClienCmd creates whoami command" {
     const gpa = std.testing.allocator;
-    const str = "/" ++ ClientCmd.Whoami.CMD;
+    const str = "/" ++ ClientToken.Cmd.Whoami.CMD;
 
     const token = try ClientToken.fromStringAlloc(gpa, str);
     defer token.deinit(gpa);
 
-    try std.testing.expectEqual(ClientCmd.Whoami{}, token.cmd.whoami);
+    try std.testing.expectEqual(ClientToken.Cmd.Whoami{}, token.cmd.whoami);
 }
 
 test "ClientCmd creates nickname command" {
     const gpa = std.testing.allocator;
     const expectedNickname = "Vasyan";
-    const str = "/" ++ ClientCmd.Nickname.CMD ++ " " ++ expectedNickname ++ " ";
+    const str = "/" ++ ClientToken.Cmd.Nickname.CMD ++ " " ++ expectedNickname ++ " ";
 
     const token = try ClientToken.fromStringAlloc(gpa, str);
     defer token.deinit(gpa);
 
-    try std.testing.expectEqualDeep(ClientCmd.Nickname{
+    try std.testing.expectEqualDeep(ClientToken.Cmd.Nickname{
         .nickname = expectedNickname,
     }, token.cmd.nickname);
 }
