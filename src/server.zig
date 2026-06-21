2d04458908a65367bb61579b0e7e082e94f891b1
Niakr1s <pavel2188@gmail.com>
2026-06-21 21:59:18 +0300

    Broadcaster refactor remained

--- src/server.zig
+++ src/server.zig
@@ -73,15 +73,28 @@ pub fn Server(comptime ConnectionProducer: type, Connection: type) type {
         connectionProducer: *ConnectionProducer,
         group: std.Io.Group = .init,
 
-        connections: std.StringArrayHashMapUnmanaged(User) = .empty,
+        connections: std.StringArrayHashMapUnmanaged(*User) = .empty,
         connectionsMu: std.Io.Mutex = .init,
 
         const User = struct {
             nickname: []const u8,
             connection: *Connection,
 
+            fn init(gpa: std.mem.Allocator, nickname: []const u8, connection: *Connection) !*User {
+                var user = try gpa.create(User);
+                user.nickname = try gpa.dupe(u8, nickname);
+                user.connection = connection;
+                return user;
+            }
+
             fn deinit(self: *@This(), gpa: std.mem.Allocator) void {
                 gpa.free(self.nickname);
+            }
+
+            /// Sets nickname. Frees old nickname. Dupes new nckname.
+            fn setNickname(self: *@This(), gpa: std.mem.Allocator, newNickname: []const u8) !void {
+                gpa.free(self.nickname);
+                self.nickname = try gpa.dupe(u8, newNickname);
             }
         };
 
@@ -111,11 +124,11 @@ pub fn Server(comptime ConnectionProducer: type, Connection: type) type {
         }
 
         fn handleConnection(self: *Self, gpa: std.mem.Allocator, io: std.Io, connection: *Connection) !void {
-            const user = try self.registerUser(gpa, io, connection) orelse return error.Unreachable;
-            std.debug.print("{s}\n", .{user.nickname});
+            const user = try self.registerUser(gpa, io, connection);
+            try self.startMessaging(gpa, io, user);
         }
 
-        fn registerUser(self: *Self, gpa: std.mem.Allocator, io: std.Io, connection: *Connection) !?User {
+        fn registerUser(self: *Self, gpa: std.mem.Allocator, io: std.Io, connection: *Connection) !*User {
             var readBuf: [256]u8 = undefined;
             var writeBuf: [256]u8 = undefined;
 
@@ -135,10 +148,7 @@ pub fn Server(comptime ConnectionProducer: type, Connection: type) type {
                                     continue;
                                 }
 
-                                const user = User{
-                                    .nickname = try gpa.dupe(u8, token.cmd.nickname.nickname),
-                                    .connection = connection,
-                                };
+                                const user = try User.init(gpa, token.cmd.nickname.nickname, connection);
                                 try self.connections.put(gpa, user.nickname, user);
                                 return user;
                             },
@@ -148,7 +158,102 @@ pub fn Server(comptime ConnectionProducer: type, Connection: type) type {
                     else => {},
                 }
             }
-            return null;
+            return error.Unreachable;
+        }
+
+        fn startMessaging(self: *Self, gpa: std.mem.Allocator, io: std.Io, user: *User) !void {
+            defer {
+                const removed = self.connections.swapRemove(user.nickname);
+                if (!removed) {
+                    std.debug.print("[{s}]: Error: wasn't removed from connections\n", .{user.nickname});
+                }
+            }
+
+            const helloLine = try std.fmt.allocPrint(gpa, "{s}: joined\n", .{user.nickname});
+            defer gpa.free(helloLine);
+            // try Broadcaster.broadcastToAll(io, self.connections.values(), helloLine);
+
+            var readBuf: [256]u8 = undefined;
+            // var reader = connection.stream.reader(io, &readBuf);
+            //
+            while (true) {
+                if (user.connection.readLine(gpa, io, &readBuf) catch |err| {
+                    const errLine = try std.fmt.allocPrint(gpa, "Error: {any}\n", .{err});
+                    defer gpa.free(errLine);
+                    // try Broadcaster.broadcastToOne(io, connection, errLine);
+
+                    continue;
+                }) |line| {
+                    const token = tokens.ClientToken.fromStringAlloc(gpa, line) catch |err| {
+                        switch (err) {
+                            error.EmptyString, error.UnknownClientCmd => {
+                                const errLine = try std.fmt.allocPrint(gpa, "Error: {any}\n", .{err});
+                                defer gpa.free(errLine);
+                                // try Broadcaster.broadcastToOne(io, connection, errLine);
+
+                                continue;
+                            },
+                            error.OutOfMemory => return err,
+                        }
+                    }; // handle errors
+                    defer token.deinit(gpa);
+
+                    switch (token) {
+                        .msg => |msg| {
+                            const fullLine = try std.fmt.allocPrint(gpa, "{s}: {s}\n", .{ user.nickname, msg.msg });
+                            defer gpa.free(fullLine);
+                            // try Broadcaster.broadcastToAllExceptOne(io, self.connections.values(), fullLine, connection.nickname);
+                        },
+                        .cmd => |cmd| {
+                            switch (cmd) {
+                                .whoami => {
+                                    const whoamiLine = try std.fmt.allocPrint(gpa, "> You are {s}\n", .{user.nickname});
+                                    defer gpa.free(whoamiLine);
+                                    // try Broadcaster.broadcastToOne(io, user, whoamiLine);
+                                },
+                                .who => {
+                                    const whoLine = try std.fmt.allocPrint(gpa, "> Clients: {any}\n", .{self.connections.values()});
+                                    defer gpa.free(whoLine);
+
+                                    var stringBuilder = try std.ArrayList(u8).initCapacity(gpa, user.nickname.len * 2);
+
+                                    // TODO: maybe I need to sort them...
+                                    for (self.connections.values()) |conn| {
+                                        try stringBuilder.appendSlice(gpa, "> ");
+                                        try stringBuilder.appendSlice(gpa, conn.nickname);
+                                        try stringBuilder.appendSlice(gpa, "\n");
+                                    }
+
+                                    // try Broadcaster.broadcastToOne(io, connection, stringBuilder.items);
+                                },
+                                .nickname => {
+                                    const newNickname = cmd.nickname.nickname;
+                                    const oldNickname = user.nickname;
+                                    if (!self.connections.contains(newNickname)) {
+                                        const removed = self.connections.swapRemove(oldNickname);
+                                        std.debug.print("removed {s} = {any}, connections count = {d}\n", .{ oldNickname, removed, self.connections.count() });
+
+                                        // this needs to be before setNickname call
+                                        const nicknameLine = try std.fmt.allocPrint(gpa, "> {s} is known as {s} now\n", .{ oldNickname, newNickname });
+                                        defer gpa.free(nicknameLine);
+
+                                        try user.setNickname(gpa, newNickname);
+                                        try self.connections.put(gpa, user.nickname, user);
+
+                                        // try Broadcaster.broadcastToAll(io, self.connections.values(), nicknameLine);
+                                    }
+                                },
+                            }
+                        },
+                    }
+                } else {
+                    break;
+                }
+            }
+            const disconnectedLine = try std.fmt.allocPrint(gpa, "{s}: disconnected\n", .{user.nickname});
+            defer gpa.free(disconnectedLine);
+            // try Broadcaster.broadcastToAllExceptOne(io, self.connections.values(), disconnectedLine, user.nickname);
+            // std.debug.print("[{s}]: disconnected\n", .{connection.nickname});
         }
     };
 }
