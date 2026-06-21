5a0da289b4313d5ba76b5e647a350d14c9799d45
Niakr1s <pavel2188@gmail.com>
2026-06-21 06:49:56 +0300

    added ClientTokenReader

--- src/tokens.zig
+++ src/tokens.zig
@@ -55,3 +55,39 @@ test "ClientToken creates a message token" {
 
     try std.testing.expectEqualStrings(str, token.msg.msg);
 }
+
+const ClientTokenReader = struct {
+    reader: *std.Io.Reader,
+
+    fn next(self: ClientTokenReader, gpa: std.mem.Allocator) !?ClientToken {
+        const line = try self.reader.takeDelimiter('\n') orelse return null;
+        return try ClientToken.fromStringAlloc(gpa, line);
+    }
+};
+
+test "ClientTokenReader reads all lines" {
+    const gpa = std.testing.allocator;
+
+    const line1 = "hello world";
+    const line2 = "bye world";
+    const line = line1 ++ "\n" ++ line2;
+
+    var reader = std.Io.Reader.fixed(line);
+    var tokenReader = ClientTokenReader{
+        .reader = &reader,
+    };
+
+    const gotLine1 = try tokenReader.next(gpa) orelse return error.GotNullLine;
+    defer gotLine1.deinit(gpa);
+    try std.testing.expectEqualStrings(line1, gotLine1.msg.msg);
+
+    const gotLine2 = try tokenReader.next(gpa) orelse return error.GotNullLine;
+    defer gotLine2.deinit(gpa);
+    try std.testing.expectEqualStrings(line2, gotLine2.msg.msg);
+
+    const gotEmptyLine1 = try tokenReader.next(gpa);
+    try std.testing.expectEqual(null, gotEmptyLine1);
+
+    const gotEmptyLine2 = try tokenReader.next(gpa);
+    try std.testing.expectEqual(null, gotEmptyLine2);
+}
